1652a7b98fa06321a51fc8a3416a6512a2a9afab
“Reshma <3areshma@email.com>
2022-08-03 19:55:57 -0500

    fs function drill 2 callback

--- stretch-excercise/fs-add-zero/addToZero.js
+++ stretch-excercise/fs-add-zero/addToZero.js
@@ -2,3 +2,16 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+// Given an array of numbers, console.log true if any two numbers in the array add to zero, otherwise log false.
+// [1, 4, 11, 2, 37, -4] should log true because 4 and -4 add to 0.
+let value = false;
+for (let i = 0; i < array.length; i++){
+    for (let j = 0; j < array.length; j++){
+        if (i!== j){
+            if(array[i] + array[j] ===0){
+               value = true;
+            }
+        }
+    }
+}
+console.log(value)
